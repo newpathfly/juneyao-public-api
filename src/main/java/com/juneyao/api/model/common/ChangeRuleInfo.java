a13e1a4cede8e4cb3d0d641162a7b808d8485280
Tiger Wang <tigerwang@outlook.com>
2021-12-10 13:15:53 -0500

    wip

--- src/main/java/com/juneyao/api/model/common/ChangeRuleInfo.java
+++ src/main/java/com/juneyao/api/model/common/ChangeRuleInfo.java
@@ -1,9 +1,26 @@
 package com.juneyao.api.model.common;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class ChangeRuleInfo {
 
     private ChangeRuleInfo() {
     }
 
-    // @todo
+    @JsonProperty("FlightTimeCondition")
+    String flightTimeCondition;
+
+    @JsonProperty("TimeConditionStart")
+    @NotBlank
+    String timeConditionStart;
+
+    @JsonProperty("TimeConditionEnd")
+    String timeConditionEnd;
+
+    @JsonProperty("Fee")
+    @NotNull
+    Integer fee;
 }
